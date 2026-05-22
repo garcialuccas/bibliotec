39eedf87ff0d727ebaf9ad79d1f1f21abe0ac07c
LUCCAS GARCIA MELO <54568203821@info.local>
2026-05-22 16:51:00 -0300

    primeiro commit para o bibliotec

---
+++ Models/Reserva.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bibliotec.Models
+{
+    public class Reserva
+    {
+        
+    }
+}
